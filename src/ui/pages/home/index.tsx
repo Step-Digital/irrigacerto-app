6e43533c7006c55d8a131084b536e400a5c8bd2a
Liandro Silva <liandro.silva@outlook.com>
2023-11-28 10:08:06 -0300

    autologin

--- src/ui/pages/home/index.tsx
+++ src/ui/pages/home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "core-js/stable/atob";
 import { ActivityIndicator, Platform, StyleSheet, View } from "react-native";
 import * as S from "./style";
@@ -14,11 +14,8 @@ import { useMutation } from "@tanstack/react-query";
 import { useNavigation } from "@react-navigation/native";
 import { NavigationProps } from "../../routes/types/StackNavigationProps";
 import { CacheDomain } from "../../../core/domain/cache.domain";
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { jwtDecode } from "jwt-decode";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { API } from "../../../core/services/axios.service";
-
-
 
 type HomeProps = {
   auth: AuthDomain;
@@ -27,48 +24,48 @@ type HomeProps = {
 
 export const HomeScreen: React.FC<HomeProps> = ({ auth, cache }) => {
   const navigation = useNavigation<NavigationProps>();
-
-  // const autoLogin = useMutation({
-  //   mutationFn: () => {
-  //     return cache.get({ key: "@token" });
-  //   },
-  //   onSuccess: (data) => {
-  //     if (data !== null) {
-  //       return navigation.navigate("HomeLogged");
-  //     }
-  //   },
-  // });
-
-  // useEffect(() => {
-  //   autoLogin.mutate();
-  // }, []);
-
-  function parseJwt (token) {
-    var base64Url = token.split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-
-    return JSON.parse(jsonPayload);
-}
+  const [autoLoginLoading, setAutoLoadingLoading] = useState(false);
 
   useEffect(() => {
-    AsyncStorage.getItem("@token").then((token) => {
-      (async () => {        
-        const accessToken = JSON.parse(token).refreshToken
-        console.log(accessToken)
-        await API.post("/auth/refresh", {
-          refreshToken: JSON.parse(token).refreshToken
-        }).then((newToken) => {
-          AsyncStorage.setItem("@token", JSON.stringify(newToken));
-          return navigation.navigate("HomeLogged");
-        });
-      })()
-    }).catch((error) => {
-      return navigation.navigate("Home");
-    })
-  }, [])
+    AsyncStorage.getItem("@token")
+      .then((token) => {
+        (async () => {
+          setAutoLoadingLoading(true);
+          const refreshToken = JSON.parse(token).refreshToken;
+          const accessToken = JSON.parse(token).accessToken;
+          await API.post(
+            "/auth/refresh",
+            {
+              refreshToken,
+            },
+            {
+              headers: {
+                Authorization: "Bearer " + accessToken,
+              },
+            }
+          )
+            .then((newToken) => {
+              AsyncStorage.setItem(
+                "@token",
+                JSON.stringify(newToken.data.data)
+              );
+            })
+            .then(() => {
+              setTimeout(() => {
+                setAutoLoadingLoading(false);
+                return navigation.navigate("HomeLogged");
+              }, 1000);
+            });
+        })();
+      })
+      .catch((error) => {
+        setAutoLoadingLoading(false);
+        return navigation.navigate("Home");
+      })
+      .finally(() => {
+        setAutoLoadingLoading(false);
+      });
+  }, []);
 
   return (
     <S.StyledView>
@@ -87,68 +84,80 @@ export const HomeScreen: React.FC<HomeProps> = ({ auth, cache }) => {
         />
       </S.StyledHeader>
       <S.StyledContent>
-        {/* {autoLogin.isLoading && (
-          <ActivityIndicator size={"large"} color={"#00344A"} />
-        )} */}
-        {/* {!autoLogin.isLoading && ( */}
-        <>
-          <View
+        {autoLoginLoading && (
+          <ActivityIndicator
             style={{
-              paddingHorizontal: 16,
+              position: "absolute",
+              flex: 1,
+              zIndex: 9999999,
+              height: "100%",
+              width: "100%",
             }}
-          >
-            <Image
-              style={localStyles.sponsorsImage}
-              source={require("../../../../assets/sponsors.png")}
-              placeholder={"Sponsors"}
-              contentFit="fill"
-              transition={1000}
-            />
-            <Typography color="gray-8" size="small" weight="regular" style={{ textAlign: 'center', marginTop: 12, fontSize: 14 }}>
-              Desenvolvido por
-            </Typography>
-            <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
-
+            size={"large"}
+            color={"#00344A"}
+          />
+        )}
+        {!autoLoginLoading && (
+          <>
+            <View
+              style={{
+                paddingHorizontal: 16,
+              }}
+            >
               <Image
-                source={require("../../../../assets/Incaper.png")}
-                contentFit="contain"
+                style={localStyles.sponsorsImage}
+                source={require("../../../../assets/sponsors.png")}
+                placeholder={"Sponsors"}
+                contentFit="fill"
                 transition={1000}
+              />
+              <Typography
+                color="gray-8"
+                size="small"
+                weight="regular"
+                style={{ textAlign: "center", marginTop: 12, fontSize: 14 }}
+              >
+                Desenvolvido por
+              </Typography>
+              <View
                 style={{
-                  width: 100,
-                  height: 40,
-                  
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "center",
                 }}
+              >
+                <Image
+                  source={require("../../../../assets/Incaper.png")}
+                  contentFit="contain"
+                  transition={1000}
+                  style={{
+                    width: 100,
+                    height: 40,
+                  }}
                 />
-                </View>
-          </View>
-          <S.StyledActions>
-            <Button
-              bg-color="positive"
-              onPress={() => navigation.navigate("Signup")}
-            >
-              <Typography color="pure-white" size="large" weight="bold">
-                {strings.userNotLoggedIn.createAccount}
-              </Typography>
-            </Button>
-            <Button
-              bg-color="transparent"
-              onPress={() => navigation.navigate("Login")}
-            >
-              <Typography color="neutral-4" size="large" weight="bold">
-                {strings.userNotLoggedIn.haveRegistration}
-              </Typography>
-            </Button>
-          </S.StyledActions>
-        </>
-        {/* )} */}
+              </View>
+            </View>
+            <S.StyledActions>
+              <Button
+                bg-color="positive"
+                onPress={() => navigation.navigate("Signup")}
+              >
+                <Typography color="pure-white" size="large" weight="bold">
+                  {strings.userNotLoggedIn.createAccount}
+                </Typography>
+              </Button>
+              <Button
+                bg-color="transparent"
+                onPress={() => navigation.navigate("Login")}
+              >
+                <Typography color="neutral-4" size="large" weight="bold">
+                  {strings.userNotLoggedIn.haveRegistration}
+                </Typography>
+              </Button>
+            </S.StyledActions>
+          </>
+        )}
       </S.StyledContent>
-      {/* {autoLogin.isError && (
-        <FlashMessage
-          title="Efetue o login para acessar o sistema"
-          show
-          duration={3000}
-        />
-      )} */}
     </S.StyledView>
   );
 };
